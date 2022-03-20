a5b9b973248a4ce533b183d3a22a3a8d9ce4f22a
ronsider <ronsider2@gmail.com>
2022-03-20 18:42:28 +0200

    added cpp file

---
+++ Notebook.cpp
@@ -0,0 +1,16 @@
+#include "Notebook.hpp"
+
+using namespace ariel;
+void Notebook::write(const unsigned int page, const unsigned int row,const unsigned int columun,const Direction d,const std::string s)
+{
+
+}
+std::string Notebook::read(const unsigned int page,const unsigned int row,const unsigned int columun,const Direction d,const unsigned int l)const
+{
+    return "";
+}
+void Notebook::erase(const unsigned int page, const unsigned int row,const unsigned int columun,const Direction d,const unsigned int l)
+{
+
+}
+
